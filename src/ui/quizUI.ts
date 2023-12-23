74b642284b4ae8c424884ec5bb6b16ac405b255e
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2023-12-23 02:27:02 -0800

    Fixed GUI

--- src/ui/quizUI.ts
+++ src/ui/quizUI.ts
@@ -14,6 +14,7 @@ export default class QuizUI {
 	open() {
 		this.allMarkdownFiles = this.app.vault.getMarkdownFiles();
 		this.noteNames = this.allMarkdownFiles.map(file => file.basename);
+		this.displaySelectedNotes();
 		this.showSearchBar();
 	}
 
@@ -49,25 +50,60 @@ export default class QuizUI {
 
 		this.container.innerHTML = "";
 
-		this.selectedNotes.forEach((selectedNote) => {
-			const selectedNoteBox = document.createElement("div");
-			selectedNoteBox.classList.add("selected-note-box");
-			selectedNoteBox.textContent = selectedNote.basename;
-			this.container?.appendChild(selectedNoteBox);
-		});
-
-		if (this.container) {
-			this.container.style.position = "fixed";
-			this.container.style.top = "20px"; // Adjust top position as needed
-			this.container.style.left = "20px"; // Adjust left position as needed
-			this.container.style.zIndex = "1000"; // Ensure it's above other elements
-			this.container.style.width = "300px"; // Set a fixed width
-			this.container.style.maxHeight = "400px"; // Set a maximum height
+		/*if (this.container) {
+			this.container.style.maxHeight = "70vh";
 			this.container.style.overflowY = "auto"; // Add a scrollbar if content exceeds the maximum height
-			this.container.style.backgroundColor = "#ebecf0"; // Trello-like background color
+			this.container.style.backgroundColor = "#ebecf0";
 			this.container.style.padding = "10px"; // Padding
 			this.container.style.border = "1px solid #ccc"; // Border for visibility
 			this.container.style.borderRadius = "5px"; // Rounded corners
+			this.container.style.display = "flex";
+			this.container.style.flexDirection = "column"; // Align children in a column
+			this.container.style.justifyContent = "space-between"; // Space between items
+		}*/
+
+		if (this.container) {
+			this.container.style.position = "fixed";
+			this.container.style.top = "50%"; // Center vertically
+			this.container.style.left = "50%"; // Center horizontally
+			this.container.style.transform = "translate(-50%, -50%)"; // Centering trick
+			this.container.style.zIndex = "1";
+			this.container.style.width = "800px";
+			this.container.style.height = "80vh"; // Set a maximum height based on viewport height
+			this.container.style.overflow = "hidden"; // Hide overflow to prevent buttons from being pushed down
+
+			// Create a new section for buttons
+			const buttonSection = document.createElement("div");
+			buttonSection.style.position = "absolute";
+			buttonSection.style.bottom = "0"; // Align to the bottom
+			buttonSection.style.width = "100%";
+
+			// Add buttons to the button section
+			const button1 = document.createElement("button");
+			button1.textContent = "Button 1";
+			buttonSection.appendChild(button1);
+
+			const button2 = document.createElement("button");
+			button2.textContent = "Button 2";
+			buttonSection.appendChild(button2);
+
+			// Append the button section to the container
+			this.container?.appendChild(buttonSection);
+
+			// Create a new section for elements
+			const elementsSection = document.createElement("div");
+			elementsSection.style.overflowY = "auto"; // Make this section scrollable
+			elementsSection.style.height = "calc(100% - 40px)"; // Adjust the height as needed
+
+			// Append the elements section to the container
+			this.container.appendChild(elementsSection);
+
+			this.selectedNotes.forEach((selectedNote) => {
+				const selectedNoteBox = document.createElement("div");
+				selectedNoteBox.classList.add("selected-note-box");
+				selectedNoteBox.textContent = selectedNote.basename;
+				elementsSection.appendChild(selectedNoteBox);
+			});
 		}
 
 		const noteBoxes = document.querySelectorAll(".selected-note-box");
@@ -116,6 +152,12 @@ class SearchBar extends FuzzySuggestModal<string> {
 	constructor(app: App, noteNames: string[]) {
 		super(app);
 		this.noteNames = noteNames;
+	}
+
+	onOpen() {
+		super.onOpen();
+		this.modalEl.style.top = "20%";
+		this.modalEl.style.left = "30%";
 	}
 
 	setCallback(callback: (selectedItem: string, evt: MouseEvent | KeyboardEvent) => void): void {
