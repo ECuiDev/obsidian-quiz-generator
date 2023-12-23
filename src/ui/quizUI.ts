38607498f36bfc85fd2f2ea8cdc7988801e0b8f5
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2023-12-23 01:43:27 -0800

    Temporary GUI fix

--- src/ui/quizUI.ts
+++ src/ui/quizUI.ts
@@ -5,6 +5,7 @@ export default class QuizUI {
 	private noteNames: string[];
 	private allMarkdownFiles: TFile[];
 	private selectedNotes: TFile[] = [];
+	private container: HTMLDivElement | null = null;
 
 	constructor(app: App) {
 		this.app = app;
@@ -21,14 +22,14 @@ export default class QuizUI {
 	}
 
 	private showSearchBar() {
-		const modal = new CustomFuzzySuggestModal(this.app, this.noteNames);
+		const modal = new SearchBar(this.app, this.noteNames);
 
 		modal.setCallback((selectedItem: string) => {
 			const selectedNote = this.getNoteByName(selectedItem);
 
 			if (selectedNote) {
 				this.selectedNotes.push(selectedNote);
-				this.showSelectedNoteBox(selectedNote);
+				this.displaySelectedNotes();
 				modal.close();
 				this.showSearchBar();
 			}
@@ -39,25 +40,66 @@ export default class QuizUI {
 		modal.open();
 	}
 
-	private showSelectedNoteBox(selectedNote: TFile) {
-		const selectedNoteBox = document.createElement("div");
-		selectedNoteBox.classList.add("selected-note-box");
-		selectedNoteBox.textContent = selectedNote.basename;
+	private displaySelectedNotes() {
+		if (!this.container) {
+			this.container = document.createElement("div");
+			this.container.id = "selected-notes-container";
+			document.body.appendChild(this.container);
+		}
 
-		document.querySelector("#container")?.appendChild(selectedNoteBox);
+		this.container.innerHTML = "";
+
+		this.selectedNotes.forEach((selectedNote) => {
+			const selectedNoteBox = document.createElement("div");
+			selectedNoteBox.classList.add("selected-note-box");
+			selectedNoteBox.textContent = selectedNote.basename;
+			this.container?.appendChild(selectedNoteBox);
+		});
+
+		if (this.container) {
+			this.container.style.position = "fixed";
+			this.container.style.top = "20px"; // Adjust top position as needed
+			this.container.style.left = "20px"; // Adjust left position as needed
+			this.container.style.zIndex = "1000"; // Ensure it's above other elements
+			this.container.style.width = "300px"; // Set a fixed width
+			this.container.style.maxHeight = "400px"; // Set a maximum height
+			this.container.style.overflowY = "auto"; // Add a scrollbar if content exceeds the maximum height
+			this.container.style.backgroundColor = "#ebecf0"; // Trello-like background color
+			this.container.style.padding = "10px"; // Padding
+			this.container.style.border = "1px solid #ccc"; // Border for visibility
+			this.container.style.borderRadius = "5px"; // Rounded corners
+		}
+
+		const noteBoxes = document.querySelectorAll(".selected-note-box");
+		noteBoxes.forEach((noteBox) => {
+			const boxElement = noteBox as HTMLElement; // Explicitly cast to HTMLElement
+			if (boxElement) {
+				boxElement.style.backgroundColor = "#343434"; // Background color
+				boxElement.style.padding = "10px"; // Padding
+				boxElement.style.marginBottom = "5px"; // Adjust margin as needed
+				boxElement.style.borderRadius = "5px"; // Rounded corners
+				boxElement.style.border = "1px solid #ddd"; // Border
+			}
+		});
 	}
 
 	private getNoteByName(noteName: string): TFile | null {
 		return this.allMarkdownFiles.find(file => file.basename === noteName) || null;
 	}
 
-	private handleKeyPress(event: KeyboardEvent, modal: CustomFuzzySuggestModal) {
+	private getRemainingNoteNames(): string[] {
+		return this.allMarkdownFiles
+			.filter((file) => !this.selectedNotes.some((selectedNote) => selectedNote.basename === file.basename))
+			.map((file) => file.basename);
+	}
+
+	private handleKeyPress(event: KeyboardEvent, modal: SearchBar) {
 		if (event.code === "Escape") {
 			this.terminateProcess(modal);
 		}
 	}
 
-	private terminateProcess(modal: CustomFuzzySuggestModal) {
+	private terminateProcess(modal: SearchBar) {
 		if (modal) {
 			console.log("Process terminated by user.");
 			modal.close();
@@ -67,7 +109,7 @@ export default class QuizUI {
 
 }
 
-class CustomFuzzySuggestModal extends FuzzySuggestModal<string> {
+class SearchBar extends FuzzySuggestModal<string> {
 	private callback: ((selectedItem: string, evt: MouseEvent | KeyboardEvent) => void) | null = null;
 	private readonly noteNames: string[];
 
