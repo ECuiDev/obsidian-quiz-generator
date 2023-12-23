144d9ea70244703e9a3f0874c4ce6da548cc3d7c
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2023-12-23 15:14:19 -0800

    Added settings

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { App, Plugin } from "obsidian";
 import QuizUI from "./ui/quizUI";
+import QuizSettingsTab from "./ui/settingsUI";
 
 export default class QuizGenerator extends Plugin {
+	settings: QuizSettings;
+
 	async onload() {
 		this.addCommand({
 			id: "open-generator-gui",
@@ -10,6 +13,35 @@ export default class QuizGenerator extends Plugin {
 				new QuizUI(this.app).open();
 			},
 		});
+
+		await this.loadSettings();
+		this.addSettingTab(new QuizSettingsTab(this.app, this));
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 
 }
+
+interface QuizSettings {
+	numberOfMultipleChoice: number;
+	numberOfTrueFalse: number;
+	numberOfShortAnswer: number;
+	generateMultipleChoice: boolean;
+	generateTrueFalse: boolean;
+	generateShortAnswer: boolean;
+}
+
+const DEFAULT_SETTINGS: Partial<QuizSettings> = {
+	numberOfMultipleChoice: 1,
+	numberOfTrueFalse: 1,
+	numberOfShortAnswer: 1,
+	generateMultipleChoice: true,
+	generateTrueFalse: true,
+	generateShortAnswer: true
+};
---
+++ src/ui/settingsUI.ts
@@ -0,0 +1,90 @@
+import { App, PluginSettingTab, Setting} from "obsidian";
+import QuizGenerator from "../main";
+
+export default class QuizSettingsTab extends PluginSettingTab {
+	plugin: QuizGenerator;
+
+	constructor(app: App, plugin: QuizGenerator) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display() {
+		let { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Generate Multiple Choice Questions")
+			.setDesc("Do you want to generate multiple choice questions?")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.generateMultipleChoice).onChange((value) => {
+						this.plugin.settings.generateMultipleChoice = value;
+						this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Number of Multiple Choice Questions")
+			.setDesc("How many multiple choice questions do you want to generate?")
+			.addSlider((slider) =>
+				slider
+					.setValue(this.plugin.settings.numberOfMultipleChoice)
+					.setLimits(1, 10, 1)
+					.onChange((value) => {
+						this.plugin.settings.numberOfMultipleChoice = value;
+						this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Generate True/False Questions")
+			.setDesc("Do you want to generate true/false questions?")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.generateTrueFalse).onChange((value) => {
+					this.plugin.settings.generateTrueFalse = value;
+					this.plugin.saveSettings();
+				})
+			);
+
+		new Setting(containerEl)
+			.setName("Number of True/False Questions")
+			.setDesc("How many true/false questions do you want to generate?")
+			.addSlider((slider) =>
+				slider
+					.setValue(this.plugin.settings.numberOfTrueFalse)
+					.setLimits(1, 10, 1)
+					.onChange((value) => {
+						this.plugin.settings.numberOfTrueFalse = value;
+						this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Generate Short Answer Questions")
+			.setDesc("Do you want to generate short answer questions?")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.generateShortAnswer).onChange((value) => {
+					this.plugin.settings.generateShortAnswer = value;
+					this.plugin.saveSettings();
+				})
+			);
+
+		new Setting(containerEl)
+			.setName("Number of Multiple Choice Questions")
+			.setDesc("How many multiple choice questions do you want to generate?")
+			.addSlider((slider) =>
+				slider
+					.setValue(this.plugin.settings.numberOfShortAnswer)
+					.setLimits(1, 10, 1)
+					.onChange((value) => {
+						this.plugin.settings.numberOfShortAnswer = value;
+						this.plugin.saveSettings();
+					})
+			);
+	}
+
+}
