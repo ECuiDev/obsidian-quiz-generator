4b435c812f4af523107dab7aeb8309b8a2b3e218
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2024-01-09 16:04:52 -0800

    Question saving format

--- src/service/questionSaver.ts
+++ src/service/questionSaver.ts
@@ -1,4 +1,4 @@
-import { App, Notice, normalizePath } from "obsidian";
+import {App, Notice, TFile, normalizePath, requestUrl} from "obsidian";
 import { ParsedMCQ, ParsedTF, ParsedSA } from "../utils/types";
 import QuizGenerator from "../main";
 
@@ -8,6 +8,9 @@ export default class QuestionSaver {
 	private questions: (ParsedMCQ | ParsedTF | ParsedSA)[];
 	private readonly fileName: string;
 	private tags: string;
+	private path: string;
+	private spacedRepetitionFile: TFile;
+	private calloutFile: TFile;
 
 	constructor(app: App, plugin: QuizGenerator, questions: (ParsedMCQ | ParsedTF | ParsedSA)[], fileName: string, tags: string) {
 		this.app = app;
@@ -18,23 +21,93 @@ export default class QuestionSaver {
 	}
 
 	async saveQuestions() {
-		try {
-			await this.app.vault.create(this.plugin.settings.questionSavePath.trim() + `/${this.fileName}`, "");
-		} catch (error) {
-			new Notice("Non-existent path. Questions saved in vault root folder.");
+		this.path = normalizePath(this.plugin.settings.questionSavePath.trim() + `/${this.fileName}`);
+
+		if (this.plugin.settings.saveForSpacedRepetition) {
+			await this.saveForSpacedRepetition();
+		}
+		if (this.plugin.settings.saveAsCallout) {
+			await this.saveAsCallout();
 		}
 	}
 
-	private saveForSpacedRepetition() {
+	private async saveForSpacedRepetition() {
+		try {
+			this.spacedRepetitionFile = await this.app.vault.create(this.path, "");
+		} catch (error) {
+			this.spacedRepetitionFile = await this.app.vault.create("", "");
+			new Notice("Non-existent path. Questions saved in vault root folder.");
+		}
 
+		this.questions.forEach(question => {
+			this.app.vault.append(this.spacedRepetitionFile, "\n" + this.formatSpacedRepQuestion(question));
+		});
 	}
 
-	private saveAsCallout() {
+	private async saveAsCallout() {
+		try {
+			this.calloutFile = await this.app.vault.create(this.path, "");
+		} catch (error) {
+			this.calloutFile = await this.app.vault.create("", "");
+			new Notice("Non-existent path. Questions saved in vault root folder.");
+		}
 
+		this.questions.forEach(question => {
+			this.app.vault.append(this.calloutFile, "\n" + this.formatCalloutQuestion(question));
+		});
+	}
+
+	private formatSpacedRepQuestion(question: ParsedMCQ | ParsedTF | ParsedSA) {
+		if ("QuestionMC" in question) {
+			return "**Multiple Choice:** " + question.QuestionMC +
+				`\na) ${question["1"]}` + `\nb) ${question["2"]}` +
+				`\nc) ${question["3"]}` + `\nd) ${question["4"]}` +
+				`\n${this.plugin.settings.multilineSeparator}\n` +
+				this.numberToAnswer(question.Answer, question);
+		} else if ("QuestionTF" in question) {
+			return "**True/False:** " + question.QuestionTF +
+				`${this.plugin.settings.inlineSeparator}` + question.Answer;
+		} else if ("QuestionSA" in question) {
+			return "**Short Answer:** " + question.QuestionSA +
+				`${this.plugin.settings.inlineSeparator}` + question.Answer;
+		} else {
+			return "Error saving question.";
+		}
+	}
+
+	private formatCalloutQuestion(question: ParsedMCQ | ParsedTF | ParsedSA) {
+		if ("QuestionMC" in question) {
+			return `> [!question] ${question.QuestionMC}` +
+				`\n> a) ${question["1"]}` + `\n> b) ${question["2"]}` +
+				`\n> c) ${question["3"]}` + `\n> d) ${question["4"]}` +
+				`\n>> [!success]- Answer\n>> ` + this.numberToAnswer(question.Answer, question);
+		} else if ("QuestionTF" in question) {
+			return `> [!question] ${question.QuestionTF}` + `\n> True or false?` +
+				`\n>> [!success]- Answer\n>> ${question.Answer}`;
+		} else if ("QuestionSA" in question) {
+			return `> [!question] ${question.QuestionSA}` + `\n>> [!success]- Answer\n>> ${question.Answer}`;
+		} else {
+			return "> [!failure] Error saving question.";
+		}
 	}
 
 	private extractTags() {
 
+	}
+
+	private numberToAnswer(input: number, question: ParsedMCQ) {
+		switch (input) {
+			case 1:
+				return "a) " + question["1"];
+			case 2:
+				return "b) " + question["2"];
+			case 3:
+				return "c) " + question["3"];
+			case 4:
+				return "d) " + question["4"];
+			default:
+				return "Error saving answer.";
+		}
 	}
 
 }
