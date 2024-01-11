96800c41e0087397e9439eec00f06cc085c250c4
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2024-01-11 15:05:51 -0800

    Re-ordered selectorUI functions

--- src/ui/selectorUI.ts
+++ src/ui/selectorUI.ts
@@ -7,7 +7,6 @@ import NoteAdder from "./noteAdder";
 import FolderAdder from "./folderAdder";
 import "styles.css";
 import QuizUI from "./quizUI";
-import {sleep} from "openai/core";
 
 export default class SelectorUI extends Modal {
 	private readonly plugin: QuizGenerator;
@@ -54,88 +53,6 @@ export default class SelectorUI extends Modal {
 		super.onClose();
 	}
 
-	private async showNoteAdder() {
-		const modal = new NoteAdder(this.app, this.notePaths);
-
-		modal.setCallback(async (selectedItem: string) => {
-			const selectedNote = this.app.vault.getAbstractFileByPath(selectedItem);
-
-			if (selectedNote instanceof TFile) {
-				this.notePaths.remove(selectedNote.path); // remove if this causes performance issues for large vaults
-				await this.showNoteAdder();
-				const noteContents = cleanUpString(await this.app.vault.cachedRead(selectedNote));
-				this.selectedNotes.set(selectedNote.path, noteContents);
-				await this.displayNote(selectedNote);
-			}
-		});
-
-		modal.open();
-	}
-
-	private async showFolderAdder() {
-		const modal = new FolderAdder(this.app, this.folderPaths);
-
-		modal.setCallback(async (selectedItem: string) => {
-			const selectedFolder = this.app.vault.getAbstractFileByPath(selectedItem);
-
-			if (selectedFolder instanceof TFolder) {
-				this.folderPaths.remove(selectedFolder.path);
-				await this.showFolderAdder();
-				const folderNotes = selectedFolder.children
-					.filter(child => child instanceof TFile && child.extension === "md");
-
-				let folderNoteContents: string[] = [];
-
-				for (const file of folderNotes) {
-					if (file instanceof TFile) {
-						folderNoteContents.push(cleanUpString(await this.app.vault.cachedRead(file)));
-					}
-				}
-
-				this.selectedNotes.set(selectedFolder.path, folderNoteContents.join(" "));
-				await this.displayFolder(selectedFolder);
-			}
-		});
-
-		modal.open();
-	}
-
-	private displayButtons() {
-		const buttonSectionLeft = this.modalEl.createDiv("left-buttons-container");
-
-		const clear = buttonSectionLeft.createEl("button");
-		clear.addClass("selector-button", "special");
-		setIcon(clear, "book-x");
-		setTooltip(clear, "Remove all");
-
-		const buttonSectionRight = this.modalEl.createDiv("right-buttons-container");
-
-		const addNote = buttonSectionRight.createEl("button");
-		addNote.addClass("selector-button");
-		setIcon(addNote, "file-plus-2");
-		setTooltip(addNote, "Add note");
-
-		const addFolder = buttonSectionRight.createEl("button");
-		addFolder.addClass("selector-button");
-		setIcon(addFolder, "folder-plus");
-		setTooltip(addFolder, "Add folder");
-
-		const generate = buttonSectionRight.createEl("button");
-		generate.addClass("selector-button");
-		setIcon(generate, "brain-circuit");
-		setTooltip(generate, "Generate");
-
-		clear.addEventListener("click", this.clearListener);
-		addNote.addEventListener("click", this.addNoteListener);
-		addFolder.addEventListener("click", this.addFolderListener);
-		generate.addEventListener("click", this.generateListener);
-	}
-
-	private displayTokens() {
-		this.tokenSection = this.modalEl.createSpan("token-section");
-		this.tokenSection.textContent = "Prompt tokens: " + this.promptTokens;
-	}
-
 	private activateButtons() {
 		this.clearListener = async () => {
 			this.selectedNotes.clear();
@@ -180,7 +97,7 @@ export default class SelectorUI extends Modal {
 
 				console.log(this.questionsAndAnswers);
 
-				new QuizUI(this.questionsAndAnswers);
+				new QuizUI(this.app, this.questionsAndAnswers).open();
 
 				this.containerEl.style.display = "none";
 
@@ -208,6 +125,120 @@ export default class SelectorUI extends Modal {
 				new Notice("Generation cancelled because all question types are set to false.")
 			}
 		}
+	}
+
+	private displayButtons() {
+		const buttonSectionLeft = this.modalEl.createDiv("left-buttons-container");
+
+		const clear = buttonSectionLeft.createEl("button");
+		clear.addClass("selector-button", "special");
+		setIcon(clear, "book-x");
+		setTooltip(clear, "Remove all");
+
+		const buttonSectionRight = this.modalEl.createDiv("right-buttons-container");
+
+		const addNote = buttonSectionRight.createEl("button");
+		addNote.addClass("selector-button");
+		setIcon(addNote, "file-plus-2");
+		setTooltip(addNote, "Add note");
+
+		const addFolder = buttonSectionRight.createEl("button");
+		addFolder.addClass("selector-button");
+		setIcon(addFolder, "folder-plus");
+		setTooltip(addFolder, "Add folder");
+
+		const generate = buttonSectionRight.createEl("button");
+		generate.addClass("selector-button");
+		setIcon(generate, "brain-circuit");
+		setTooltip(generate, "Generate");
+
+		clear.addEventListener("click", this.clearListener);
+		addNote.addEventListener("click", this.addNoteListener);
+		addFolder.addEventListener("click", this.addFolderListener);
+		generate.addEventListener("click", this.generateListener);
+	}
+
+	private displayTokens() {
+		this.tokenSection = this.modalEl.createSpan("token-section");
+		this.tokenSection.textContent = "Prompt tokens: " + this.promptTokens;
+	}
+
+	private chooseFileName() {
+		let count = 1;
+		const folder =
+			this.app.vault.getAbstractFileByPath(normalizePath(this.plugin.settings.questionSavePath.trim()));
+
+		if (folder instanceof TFolder) {
+			const fileNames = folder.children
+				.filter(file => file instanceof TFile)
+				.map(file => file.name.toLowerCase())
+				.filter(name => name.startsWith("quiz"));
+
+			while (fileNames.includes(`quiz ${count}.md`)) {
+				count++;
+			}
+
+			this.fileName = `Quiz ${count}.md`;
+			this.validPath = true;
+		} else {
+			const rootFileNames = this.app.vault.getRoot().children
+				.filter(file => file instanceof TFile)
+				.map(file => file.name.toLowerCase())
+				.filter(name => name.startsWith("quiz"));
+
+			while (rootFileNames.includes(`quiz ${count}.md`)) {
+				count++;
+			}
+
+			this.fileName = `Quiz ${count}.md`;
+			this.validPath = false;
+		}
+	}
+
+	private async showNoteAdder() {
+		const modal = new NoteAdder(this.app, this.notePaths);
+
+		modal.setCallback(async (selectedItem: string) => {
+			const selectedNote = this.app.vault.getAbstractFileByPath(selectedItem);
+
+			if (selectedNote instanceof TFile) {
+				this.notePaths.remove(selectedNote.path); // remove if this causes performance issues for large vaults
+				await this.showNoteAdder();
+				const noteContents = cleanUpString(await this.app.vault.cachedRead(selectedNote));
+				this.selectedNotes.set(selectedNote.path, noteContents);
+				await this.displayNote(selectedNote);
+			}
+		});
+
+		modal.open();
+	}
+
+	private async showFolderAdder() {
+		const modal = new FolderAdder(this.app, this.folderPaths);
+
+		modal.setCallback(async (selectedItem: string) => {
+			const selectedFolder = this.app.vault.getAbstractFileByPath(selectedItem);
+
+			if (selectedFolder instanceof TFolder) {
+				this.folderPaths.remove(selectedFolder.path);
+				await this.showFolderAdder();
+				const folderNotes = selectedFolder.children
+					.filter(child => child instanceof TFile && child.extension === "md");
+
+				let folderNoteContents: string[] = [];
+
+				for (const file of folderNotes) {
+					if (file instanceof TFile) {
+						folderNoteContents.push(cleanUpString(await this.app.vault.cachedRead(file)));
+					}
+				}
+
+				this.selectedNotes.set(selectedFolder.path, folderNoteContents.join(" "));
+				await this.displayFolder(selectedFolder);
+			}
+		});
+
+		modal.open();
 	}
 
 	private async displayNote(note: TFile) {
@@ -265,16 +296,6 @@ export default class SelectorUI extends Modal {
 		this.tokenSection.textContent = "Prompt tokens: " + this.promptTokens;
 	}
 
-	private async loadNoteContents() {
-		const noteContents: string[] = [];
-
-		for (const noteContent of this.selectedNotes.values()) {
-			noteContents.push(noteContent);
-		}
-
-		return noteContents;
-	}
-
 	private async countNoteTokens(noteContents: string | undefined) {
 		if (typeof noteContents === "string") {
 			return Math.round(noteContents.length / 4);
@@ -283,36 +304,14 @@ export default class SelectorUI extends Modal {
 		}
 	}
 
-	private chooseFileName() {
-		let count = 1;
-		const folder =
-			this.app.vault.getAbstractFileByPath(normalizePath(this.plugin.settings.questionSavePath.trim()));
+	private async loadNoteContents() {
+		const noteContents: string[] = [];
 
-		if (folder instanceof TFolder) {
-			const fileNames = folder.children
-				.filter(file => file instanceof TFile)
-				.map(file => file.name.toLowerCase())
-				.filter(name => name.startsWith("quiz"));
-
-			while (fileNames.includes(`quiz ${count}.md`)) {
-				count++;
-			}
-
-			this.fileName = `Quiz ${count}.md`;
-			this.validPath = true;
-		} else {
-			const rootFileNames = this.app.vault.getRoot().children
-				.filter(file => file instanceof TFile)
-				.map(file => file.name.toLowerCase())
-				.filter(name => name.startsWith("quiz"));
-
-			while (rootFileNames.includes(`quiz ${count}.md`)) {
-				count++;
-			}
-
-			this.fileName = `Quiz ${count}.md`;
-			this.validPath = false;
+		for (const noteContent of this.selectedNotes.values()) {
+			noteContents.push(noteContent);
 		}
+
+		return noteContents;
 	}
 
 }
