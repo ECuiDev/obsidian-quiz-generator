084c8dda3a257076f6aa0f282b1720c222966005
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2024-02-09 02:24:24 -0800

    Finished callout parser

--- src/service/quizRevisitor.ts
+++ src/service/quizRevisitor.ts
@@ -1,17 +1,17 @@
 import { App, TFile, Notice } from "obsidian";
 import QuizGenerator from "../main";
 import QuizUI from "../ui/quizUI";
-import { ParsedQuestions, ParsedMC, ParsedTF, ParsedSA } from "../utils/types";
+import { ParsedMC, ParsedTF, ParsedSA } from "../utils/types";
 
 export default class QuizRevisitor {
 	private app: App;
 	private readonly plugin: QuizGenerator;
-	private quiz: QuizUI;
 	private questionsAndAnswers: (ParsedMC | ParsedTF | ParsedSA)[];
 
 	constructor(app: App, plugin: QuizGenerator) {
 		this.app = app;
 		this.plugin = plugin;
+		this.questionsAndAnswers = [];
 	}
 
 	public async openQuiz(): Promise<void> {
@@ -19,7 +19,9 @@ export default class QuizRevisitor {
 
 		if (currentFile instanceof TFile) {
 			const fileContents = await this.app.vault.cachedRead(currentFile);
-			console.log(JSON.stringify(fileContents));
+			console.log(fileContents);
+			this.calloutParser(fileContents);
+			new QuizUI(this.app, this.plugin, this.questionsAndAnswers).open();
 		}
 	}
 
@@ -34,39 +36,68 @@ export default class QuizRevisitor {
 	}
 
 	private calloutParser(fileContents: string): void {
-		const regexMC = />\s*\[!question]\s*(.+)\\n>\s*(.+)\\n>\s*(.+)\\n>\s*(.+)\\n>\s*(.+)\\n>>\s*\[!success]\s*(.+)\\n>>(.+?)\\n/gim;
+		const regexMC = />\s*\[!question]\s*(.+)\s*>\s*a\)\s*(.+)\s*>\s*b\)\s*(.+)\s*>\s*c\)\s*(.+)\s*>\s*d\)\s*(.+)\s*>>\s*\[!success]\s*(.+)\s*>>\s*([abcd])\)\s*(.+)/gim;
 
 		let match;
 		while ((match = regexMC.exec(fileContents)) !== null) {
-			const [, questionMC, , , choice1, choice2, choice3, choice4, answer] = match;
+			const question = match[1];
+			const choice1 = match[2].replace("a) ", "");
+			const choice2 = match[3].replace("b) ", "");
+			const choice3 = match[4].replace("c) ", "");
+			const choice4 = match[5].replace("d) ", "");
+			const answerString = match[7];
+
+			let answer = -1;
+			switch (answerString) {
+				case "a":
+					answer = 1;
+					break;
+				case "b":
+					answer = 2;
+					break;
+				case "c":
+					answer = 3;
+					break;
+				case "d":
+					answer = 4;
+					break;
+				default:
+					new Notice("Invalid multiple choice callout format.");
+					break;
+			}
+
 			this.questionsAndAnswers.push({
-				questionMC,
+				questionMC: question,
 				1: choice1,
 				2: choice2,
 				3: choice3,
 				4: choice4,
-				answer: parseInt(answer, 10)
-			});
+				answer: answer
+			} as ParsedMC);
 		}
 
-		const regexTF = />\s*\[!question]\s*(.+)\\n>\s*True or false\?\\n>>\s*\[!success]\s*(.+)\\n>>\s*(.+?)\\n/gim;
+		const regexTF = />\s*\[!question]\s*(.+)\s*>\s*True or false\?\s*>>\s*\[!success]\s*(.+)\s*>>\s*(.+)/gim;
 
 		while ((match = regexTF.exec(fileContents)) !== null) {
-			const [, questionTF, answer] = match;
+			const question = match[1];
+			const answerString = match[3];
+
 			this.questionsAndAnswers.push({
-				questionTF,
-				answer: answer.toLowerCase() === 'true'
-			});
+				questionTF: question,
+				answer: answerString.toLowerCase() === "true"
+			} as ParsedTF);
 		}
 
-		const regexSA = />\s*\[!question]\s*([^>]+?)\\n>>\s*\[!success]\s*(.+)\\n>>\s*(.+?)\\n/gim;
+		const regexSA = />\s*\[!question]\s*([^>]+)\s*>>\s*\[!success]\s*(.+)\s*>>\s*(.+)/gim;
 
 		while ((match = regexSA.exec(fileContents)) !== null) {
-			const [, questionSA, answer] = match;
+			const question = match[1];
+			const answerString = match[3];
+
 			this.questionsAndAnswers.push({
-				questionSA,
-				answer: answer.trim()
-			});
+				questionSA: question,
+				answer: answerString
+			} as ParsedSA);
 		}
 	}
 
