9ffdfa1f8a6dc913252a64aa1e0f5b21ecb81868
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2024-10-24 20:58:17 -0700

    Update Claude 3.5 Sonnet to latest version

--- src/generators/anthropic/anthropicModels.ts
+++ src/generators/anthropic/anthropicModels.ts
@@ -2,7 +2,7 @@ export const enum AnthropicTextGenModel {
 	CLAUDE_3_HAIKU = "claude-3-haiku-20240307",
 	CLAUDE_3_SONNET = "claude-3-sonnet-20240229",
 	CLAUDE_3_OPUS = "claude-3-opus-20240229",
-	CLAUDE_3_5_SONNET = "claude-3-5-sonnet-20240620",
+	CLAUDE_3_5_SONNET = "claude-3-5-sonnet-20241022",
 }
 
 export const anthropicTextGenModels: Record<AnthropicTextGenModel, string> = {
